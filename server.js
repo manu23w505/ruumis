cdf3423ead2da74e741428d5dc368864507e2633
manugtzhdz <manugtzhdz11@gmail.com>
2026-07-08 17:52:53 -0600

    Actualizacion

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'ruumis',
+    port: process.env.DB_PORT || 3306, 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
